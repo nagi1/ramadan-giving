952fe8de566ebe9a1a99162d5272fb4773711911
Ahmed Nagi <ahmedflnagi@gmail.com>
2024-02-25 11:48:41 +0200

    Refactor code for improved performance and readability

---
+++ database/migrations/2024_02_25_094543_create_giving_entries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Campaign;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('giving_entries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Campaign::class);
+            $table->string('name')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('identifier')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('giving_entries');
+    }
+};
